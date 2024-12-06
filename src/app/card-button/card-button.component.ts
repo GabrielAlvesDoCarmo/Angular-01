d4101e8c8049147438ea9d33fc283c9817d3ac11
Gabriel Alves Do Carmo <104175823+GabrielAlvesDoCarmo@users.noreply.github.com>
2024-12-06 18:10:34 -0300

    inline template

--- src/app/card-button/card-button.component.ts
+++ src/app/card-button/card-button.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button',
   standalone: false,
-  
-  templateUrl: './card-button.component.html',
+  // templateUrl: './card-button.component.html',
+  template: `
+    <div class="card-button">Adiquirir</div>
+  `,
   styleUrl: './card-button.component.scss'
 })
 export class CardButtonComponent {
--- src/app/card-roxo-button/card-roxo-button.component.ts
+++ src/app/card-roxo-button/card-roxo-button.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-roxo-button',
   standalone: false,
-  
-  templateUrl: './card-roxo-button.component.html',
+  // templateUrl: './card-roxo-button.component.html',
+  template: `<div class="card-roxo-button">Adiquirir</div>`,
   styleUrl: './card-roxo-button.component.scss'
 })
 export class CardRoxoButtonComponent {
