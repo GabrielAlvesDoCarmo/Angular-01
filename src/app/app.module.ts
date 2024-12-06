d6377df8e0373ea515a8dba86d404cfd726098ba
Gabriel Alves Do Carmo <104175823+GabrielAlvesDoCarmo@users.noreply.github.com>
2024-12-06 13:27:36 -0300

    second card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardOneComponent } from './card-one/card-one.component';
+import { CardPurpleComponent } from './card-purple/card-purple.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardOneComponent
+    CardOneComponent,
+    CardPurpleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/card-purple/card-purple.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-purple',
+  standalone: false,
+  
+  templateUrl: './card-purple.component.html',
+  styleUrl: './card-purple.component.scss'
+})
+export class CardPurpleComponent {
+
+}
