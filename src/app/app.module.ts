9f7f0fb0426831039bbaf80e38fef6abedd17461
Gabriel Alves Do Carmo <104175823+GabrielAlvesDoCarmo@users.noreply.github.com>
2024-12-06 13:44:55 -0300

    create buttons and new card module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardOneComponent } from './card-one/card-one.component';
 import { CardPurpleComponent } from './card-purple/card-purple.component';
+import { CardButtonComponent } from './card-button/card-button.component';
+import { CardRoxoButtonComponent } from './card-roxo-button/card-roxo-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardOneComponent,
-    CardPurpleComponent
+    CardPurpleComponent,
+    CardButtonComponent,
+    CardRoxoButtonComponent
   ],
   imports: [
     BrowserModule,
