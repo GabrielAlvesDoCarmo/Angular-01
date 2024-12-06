50af33e9f01982c7530d4930abd49671327d3e1d
Gabriel Alves Do Carmo <104175823+GabrielAlvesDoCarmo@users.noreply.github.com>
2024-12-06 12:37:53 -0300

    first commit

---
+++ src/app/card-one/card-one.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-one',
+  standalone: false,
+  
+  templateUrl: './card-one.component.html',
+  styleUrl: './card-one.component.scss'
+})
+export class CardOneComponent {
+
+}
